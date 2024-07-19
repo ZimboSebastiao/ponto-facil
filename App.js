1619ec7f8e97be812a34d4097fad17b2a88b7d81
Zimbo Sebastião <gloriasebastiao7@gmail.com>
2024-07-19 14:13:07 -0300

    Drawer.screen Login ocultada

--- App.js
+++ App.js
@@ -1,102 +1,101 @@
 import './gesture-handler';
 import { useState, useEffect } from "react";
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View } from 'react-native';
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { DrawerContentScrollView, DrawerItemList, createDrawerNavigator } from "@react-navigation/drawer";
 import Login from "./src/screens/Login";
 import Home from "./src/screens/Home";
-import {
-  DrawerContentScrollView,
-  DrawerItemList,
-  createDrawerNavigator,
-} from "@react-navigation/drawer";
-import {
-  FileText,
-  LogOut,
-  Settings,
-  Clock,
-  CircleX,
-  House,
-  UserRound,
-} from "lucide-react-native";
 import Perfil from './src/screens/Perfil';
 import UsuarioAvatar from './src/screens/UsuarioAvatar';
 import Relatorio from './src/screens/Relatorio';
 import Logout from './src/components/Logout';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import { FileText, LogOut, House, UserRound } from "lucide-react-native";
 
+const Stack = createNativeStackNavigator();
 const Drawer = createDrawerNavigator();
+
 function CustomDrawerContent(props) {
   return (
     <DrawerContentScrollView {...props}>
-     <UsuarioAvatar />
+      <UsuarioAvatar />
       <DrawerItemList {...props} />
     </DrawerContentScrollView>
   );
 }
 
 export default function App() {
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+
+  useEffect(() => {
+    const checkAuthStatus = async () => {
+      try {
+        const user = await AsyncStorage.getItem('usuario');
+        setIsAuthenticated(!!user); // Se o usuário existe, está autenticado
+      } catch (error) {
+        console.error("Erro ao verificar status de autenticação:", error);
+      }
+    };
+
+    checkAuthStatus();
+  }, []);
+
   return (
     <NavigationContainer>
+      <StatusBar style="auto" />
       <Drawer.Navigator
-          initialRouteName="Home"
-          screenOptions={{
-            headerStyle: { backgroundColor: "#1D1D1D" },
-            headerTintColor: "white",
+        initialRouteName="Home"
+        screenOptions={{
+          headerStyle: { backgroundColor: "#1D1D1D" },
+          headerTintColor: "white",
+        }}
+        drawerContent={(props) => <CustomDrawerContent {...props} />}
+      >
+        <Drawer.Screen
+          name="Home"
+          component={Home}
+          options={{
+            headerShown: false,
+            drawerIcon: () => <House color="#ff7938" m="$0" w="$8" h="$6" />,
           }}
-          drawerContent={(props) => <CustomDrawerContent {...props} />}
-        >
-
-          <Drawer.Screen
-            name="Home"
-            component={Home}
-            options={{
+        />
+        <Drawer.Screen
+          name="Relatório"
+          component={Relatorio}
+          options={{
             headerShown: false,
-            drawerIcon: () => (
-            <House  color="#ff7938" m="$0" w="$8" h="$6" />
-              ),
-              }}
-          />  
-          <Drawer.Screen
-            name="Relatório"
-            component={Relatorio}
-            options={{
+            drawerIcon: () => <FileText color="#ff7938" m="$0" w="$8" h="$6" />,
+          }}
+        />
+        <Drawer.Screen
+          name="Perfil"
+          component={Perfil}
+          options={{
             headerShown: false,
-            drawerIcon: () => (
-            <FileText  color="#ff7938" m="$0" w="$8" h="$6" />
-              ),
-              }}
-          />  
-            <Drawer.Screen
-              name="Perfil"
-              component={Perfil}
-              options={{
-                headerShown: false,
-                drawerIcon: () => (
-                  <UserRound color="#ff7938" m="$0" w="$8" h="$6" />
-                    ),
-              }}
-            />
-            <Drawer.Screen
-              name="Sair"
-              component={Logout}
-              options={{
-                headerShown: false,
-                drawerIcon: () => (
-                  <LogOut  color="#ff7938" m="$0" w="$8" h="$6" />
-                    ),
-              }}
-            />
-            <Drawer.Screen
-              name="Login"
-              component={Login}
-              options={{
-                headerShown: false,
-              }}
-            />
-
-    </Drawer.Navigator>
-  </NavigationContainer>
+            drawerIcon: () => <UserRound color="#ff7938" m="$0" w="$8" h="$6" />,
+          }}
+        />
+        <Drawer.Screen
+          name="Sair"
+          component={Logout}
+          options={{
+            headerShown: false,
+            drawerIcon: () => <LogOut color="#ff7938" m="$0" w="$8" h="$6" />,
+          }}
+        />
+        {!isAuthenticated && (
+          <Drawer.Screen
+            name="Login"
+            component={Login}
+            options={{
+              headerShown: false,
+            }}
+          />
+        )}
+      </Drawer.Navigator>
+    </NavigationContainer>
   );
 }
 
